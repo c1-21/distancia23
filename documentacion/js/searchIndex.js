219ec057e7c340aae014c28e4cc71f8068baec05
c1-21 <70469252+c1-21@users.noreply.github.com>
2023-05-13 00:43:12 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\saludar\u0028\u0029",
+            "name": "saludar",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020un\u0020saludo\u0020con\u0020el\u0020nombre\u0020que\u0020le\nenviamos\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_saludar"
+        },                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Funci\u00F3n\u0020que\u0020suma\u0020dos\u0020n\u00FAmeros\u0020enteros.",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\MiClase",
+            "name": "MiClase",
+            "summary": "",
+            "url": "classes/MiClase.html"
+        },                {
+            "fqsen": "\\MiClase\u003A\u003Asaludar\u0028\u0029",
+            "name": "saludar",
+            "summary": "M\u00E9todo\u0020para\u0020saludar.",
+            "url": "classes/MiClase.html#method_saludar"
+        },                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020Calculadora\nEsta\u0020clase\u0020representa\u0020una\u0020calculadora\u0020simple\u0020con\u0020un\u0020m\u00E9todo\u0020para\u0020realizar\u0020una\u0020suma.",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Asuma\u0028\u0029",
+            "name": "suma",
+            "summary": "M\u00E9todo\u0020para\u0020realizar\u0020una\u0020suma.",
+            "url": "classes/Calculadora.html#method_suma"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
